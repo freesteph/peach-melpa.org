15a44b9e2171c7e55f57673e1b9b737876f75eea
Stéphane Maniaci <stephane.maniaci@gmail.com>
2017-12-19 20:34:21 +0000

    fix: it takes a bloody screenshot

--- lib/take-screenshot.el
+++ lib/take-screenshot.el
@@ -1,18 +1,28 @@
+;; superfluous chrome
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-(defun fetch-and-load-theme (theme-name)
-  (package-refresh-contents)
-  (setq pkg (make-symbol (concat theme-name "-theme")))
-  (unless (package-installed-p pkg) (package-install pkg))
+(defun peach--install-if-necessary (theme-name)
+  "Install THEME-NAME is not already installed on the system."
+  (let
+      ((pkg-name (make-symbol (concat theme-name "-theme")))
+       (screenshoter "gnome-screenshot"))
+    (message "going to install %s" theme-name)
+    (unless (package-installed-p pkg-name)
+      (package-install pkg-name))))
 
-  (message "about to load theme")
-  (load-theme (intern theme-name) t)
-  (find-file (concat default-directory "lib/sample.js"))
-  (redisplay t)
-  (sleep-for 1)
-  (async-shell-command (concat "screencapture -x " default-directory "../app/assets/images/" theme-name ".jpg"))
-  (disable-theme theme-name)
-  (switch-to-buffer "take-screenshot.el")
-  )
+(defun fetch-and-load-theme (theme-name)
+  "Get and install THEME-NAME before taking a screenshot of it."
+  (let ((cmd-name (concat "gnome-screenshot -B -w -f " default-directory "app/assets/images/" theme-name ".jpg")))
+    (load-theme (intern theme-name) t)
+    (message "going to screenshot with %s" cmd-name)
+    (find-file (concat default-directory "lib/sample.js"))
+    (redisplay t)
+    (sleep-for 1)
+    (shell-command cmd-name nil nil)
+    (disable-theme theme-name)))
