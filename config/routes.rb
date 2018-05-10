a54f6c300fba968d75ceeee46173a0282afc5fd4
Stéphane Maniaci <stephane.maniaci@asos.com>
2018-05-10 13:58:41 +0100

    fix: only show and index themes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :themes
+  resources :themes, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
