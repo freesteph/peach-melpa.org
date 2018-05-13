6382413b04216352d0454bf5857d8e834580ab34
Stéphane <steph@steph-fedora.connect>
2018-05-13 17:43:13 +0100

    fix: use a new PEACH_ENV variable to decide how to screenshot

--- lib/take-screenshot.el
+++ lib/take-screenshot.el
@@ -7,23 +7,30 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
+(defun peach--get-screenshot-cmd ()
+  "Use the environment to figure out the screenshot command."
+  (let ((peach-env (getenv "PEACH_ENV")))
+    (cond ((string-equal "OSX" peach-env) "whatever the OSX one was")
+          ((string-equal "GNOME" peach-env) "gnome-screenshot -B -w -f ")
+          ((string-equal "AWS" peach-env) "import -window root "))))
+
 (defun peach--install-if-necessary (theme-name version)
-  "Install THEME-NAME is not already installed on the system."
-  (let (
-	(pkg (package-desc-create
-	      :name (make-symbol (concat theme-name "-theme"))
-	      :version (version-to-list version)
-	      :kind 'single
-	      :archive "melpa"
-	      ))
-	(screenshoter "gnome-screenshot"))
-    (unless (package-installed-p pkg)
-      (package-install pkg))))
+  "Install THEME-NAME at VERSION revision if not already installed on the system."
+  (let ((pkg (package-desc-create
+              :name (make-symbol (concat theme-name "-theme"))
+              :version (version-to-list version)
+              :kind 'single
+              :archive "melpa"
+              )))
+  (unless (package-installed-p pkg)
+    (package-install pkg))))
 
 (defun fetch-and-load-theme (theme-name version)
-  "Get and install THEME-NAME before taking a screenshot of it."
+  "Get and install THEME-NAME at VERSION before taking a screenshot of it."
   (peach--install-if-necessary theme-name version)
-  (let ((cmd-name (concat "DISPLAY=:13 import -window root " default-directory "app/assets/images/" theme-name ".jpg")))
+  (let* (
+         (screenshot-path (concat default-directory "app/assets/images/" theme-name ".jpg"))
+         (cmd-name (peach--get-screenshot-cmd)))
     (load-theme (intern theme-name) t)
     (toggle-frame-fullscreen)
     (find-file (concat default-directory "lib/sample.js"))
@@ -32,3 +39,6 @@
     (shell-command cmd-name nil nil)
     (disable-theme theme-name)
     (kill-emacs)))
+
+(provide 'take-screenshot)
+;;; take-screenshot.el ends here
