6f466989439f024cd195578cbb73a6c83d3e4d63
EC2 Default User <ec2-user@ip-172-31-21-37.eu-west-1.compute.internal>
2018-05-18 09:56:20 +0000

    fix: add a yet-to-be-used script to install the Iosevka font

---
+++ install-font.sh
@@ -0,0 +1,7 @@
+# don't know where to put this yet
+
+cd tmp
+curl -L -O "https://github.com/be5invis/Iosevka/releases/download/v1.14.2/01-iosevka-1.14.2.zip"
+unzip 01-iosevka-1.14.2.zip
+cd ttf
+cp *.ttf ~/.fonts
