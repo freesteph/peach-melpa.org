6d177c876487225421841ca975c6badb945c03e1
EC2 Default User <ec2-user@ip-172-31-33-66.eu-west-1.compute.internal>
2018-05-21 20:37:21 +0000

    fix: root to themes index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "themes#index"
+
   resources :themes, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
