68b36b4739fba92d71a60fae0a70c94d1f3ab2b1
Stéphane <steph@steph-fedora.connect>
2018-05-23 18:44:23 +0100

    fix: add RAILS_MASTER_KEY and capistrano deploy works

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,10 @@ set :repo_url, "git@github.com:freesteph/peach-melpa.org.git"
 
 set :tmp_dir, "/home/deploy/tmp"
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+  'RAILS_MASTER_KEY' => ENV["RAILS_MASTER_KEY"],
+  'RAILS_ENV' => 'production'
+}
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
