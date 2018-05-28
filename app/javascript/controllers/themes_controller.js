87c561c9636a3f9c87739ff75be144c4eecbd20f
Stéphane Maniaci <stephane.maniaci@gmail.com>
2018-05-28 10:28:37 +0100

    feature: reset slider value on double-click

--- app/javascript/controllers/themes_controller.js
+++ app/javascript/controllers/themes_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "stimulus";
 
+const DOUBLE_CLICK_THRESHOLD = 420; // blaze it
+
 export default class extends Controller {
+  static targets = ["slider"];
+
+  lastSliderClick = Date.now();
+
   adjustBrightness({ target: { value } }) {
     const distance = parseFloat(value, 10);
 
@@ -16,5 +22,18 @@ export default class extends Controller {
     const collection = document.getElementById("wrapper");
 
     collection.childNodes.forEach(checkVisibility);
+  }
+
+  handleDoubleClick() {
+    const diff = Date.now() - this.lastSliderClick;
+    this.lastSliderClick = Date.now();
+
+    if (diff < DOUBLE_CLICK_THRESHOLD) this.resetSlider();
+  }
+
+  resetSlider() {
+    this.sliderTarget.value = 0;
+
+    this.adjustBrightness({ target: { value: 0 } });
   }
 }
