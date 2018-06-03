9f6bcbfbee05232acef1c7203c4d3a5c85513edf
Stéphane Maniaci <stephane.maniaci@gmail.com>
2018-06-03 14:56:11 +0100

    fix: hide incomplete brightness feature behind a flag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def feature_enabled? name
+    peach_features.include? name
+  end
+
+  private
+
+  def peach_features
+    ENV
+      .select { |key| key.starts_with? "PEACH_" }
+      .keys
+      .map { |key| key.split("PEACH_").last.downcase }
+  end
 end
