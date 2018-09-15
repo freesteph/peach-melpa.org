1888af338f124819a9cefd0004f3b3abc84aa0eb
Stéphane Maniaci <stephane.maniaci@gmail.com>
2018-09-15 13:52:32 +0000

    fix: production tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.active_job.queue_adapter = :inline
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
