43724ba128e12849c335b89bd73e7a7d36db3446
Stéphane Maniaci <stephane.maniaci@gmail.com>
2018-10-04 22:49:32 +0100

    fix: a dirty, dirty multi-variants-code gallery

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -1,38 +1,30 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["modes", "screenshots"];
+  static targets = ["modes", "screenshots", "variants"];
+
+  handleVariantClick({ target }) {
+    const nodes = this.variantsTarget.querySelectorAll("li button");
+    const index = Array.from(nodes).findIndex(n => n == target);
+
+    const gallery = document.querySelector(".gallery");
+    gallery.style = `top: ${
+      gallery.style.top
+    }; left: calc(100vw * 0.75 * ${-index});`;
+  }
 
   findModeIndex(target) {
     const nodes = Array.from(this.modesTarget.querySelectorAll("li button"));
 
     return nodes.indexOf(target);
-
-  }
-
-  disableAllModes() {
-    const nodes = document.querySelectorAll(".modes li");
-
-    for (let i = 0; i < nodes.length; i++) {
-      nodes.item(i).classList = [];
-    }
-  }
-
-  selectScreenshotFor(index) {
-    const nodes = this.screenshotsTarget.querySelectorAll("img");
-
-    for (let i = 0; i < nodes.length; i++) {
-      nodes.item(i).classList = i === index ? ["active"] : [];
-    }
   }
 
   handleLangClick({ target }) {
-    this.disableAllModes();
+    const nodes = this.modesTarget.querySelectorAll("li button");
+    const index = Array.from(nodes).findIndex(n => n == target);
 
-    target.parentNode.classList = ["active"];
-
-    const activeIndex = this.findModeIndex(target);
-
-    this.selectScreenshotFor(activeIndex);
+    const gallery = document.querySelector(".gallery");
+    gallery.style = `top: calc(100vw * 0.75 * 0.75 * ${-index});
+       left: ${gallery.style.left};`;
   }
 }
