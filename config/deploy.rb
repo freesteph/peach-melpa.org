70315021302d8f97aaea4912135909bcbf47dfaf
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-02-14 10:52:16 +0000

    fix: update Capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.11.2'
+lock '~> 3.12.0'
 
 set :application, 'peach-melpa'
 set :repo_url, 'https://github.com/freesteph/peach-melpa.org.git'
