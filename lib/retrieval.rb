9e2f1dbdab9f818f97db684c2643c102f95738ef
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-07-06 16:13:47 +0200

    fix: update fetch mechanism to use HTTPS

--- lib/retrieval.rb
+++ lib/retrieval.rb
@@ -6,17 +6,22 @@ require 'rails'
 
 module PeachMelpa
   module Retrieval
-    MELPA_HOST = 'melpa.org'
+    MELPA_HOST = 'https://melpa.org'
     MELPA_ARCHIVE_NAME = '/archive.json'
     ARCHIVE_PATH = "#{Rails.root}/tmp/archive.json"
 
     def self.refresh_melpa_archive
       PeachMelpa::Log.info { 'fetching JSON archive from MELPA' }
-      archive = Net::HTTP.get(MELPA_HOST, MELPA_ARCHIVE_NAME)
+      archive = Net::HTTP.get(archive_url)
                          .encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
+
       File.write(ARCHIVE_PATH, archive)
       PeachMelpa::Log.info { "wrote #{MELPA_ARCHIVE_NAME} to #{ARCHIVE_PATH}" }
       PeachMelpa::Log.info { 'done fetching!' }
+    end
+
+    def self.archive_url
+      URI.join(MELPA_HOST, MELPA_ARCHIVE_NAME)
     end
   end
 end
--- spec/lib/retrieval_spec.rb
+++ spec/lib/retrieval_spec.rb
@@ -7,10 +7,21 @@ require './lib/retrieval'
 RSpec.describe PeachMelpa::Retrieval do
   include PeachMelpa::Retrieval
 
+  describe 'archive_url' do
+    before do
+      allow(URI).to receive(:join).and_return :url
+    end
+
+    it 'uses URI to concatenate an HTTPS URI' do
+      expect(PeachMelpa::Retrieval.archive_url).to eq :url
+    end
+  end
+
   describe 'refresh_melpa_archive' do
     before :each do
       @resp = double
       allow(@resp).to receive(:encode).and_return :response
+      allow(PeachMelpa::Retrieval).to receive(:archive_url).and_return :archive_url
       allow(Net::HTTP).to receive(:get).and_return @resp
       allow(File).to receive(:write)
     end
@@ -22,10 +33,7 @@ RSpec.describe PeachMelpa::Retrieval do
     it 'should call net open with the MELPA archive' do
       expect(Net::HTTP)
         .to have_received(:get)
-        .with(
-          PeachMelpa::Retrieval::MELPA_HOST,
-          PeachMelpa::Retrieval::MELPA_ARCHIVE_NAME
-        )
+        .with(:archive_url)
     end
 
     it 'should write the result into the tmp/archive.json file' do
